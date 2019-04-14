3474e5a4b2c9649a4a4958bb4a7d98f63a6c4721
Chris Kay <chriskay25@gmail.com>
2019-04-14 18:07:16 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,8 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+    new = []
+    i = 0 
+      while i < array.length 
+        new << yield(array[i])
+        i += 1 
+      end
 end
